e9893b36b356c063934b914e5ae43b0be734cfa0
Wellington Thiago da Silva <zatara198@gmail.com>
2019-06-25 23:12:44 -0300

    Possible moves of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -37,6 +37,10 @@ public class ChessMatch {
 		if(!board.thereIsAPiece(position)){
 			throw new ChessException("There is no piece on source position");
 		}
+		
+		if(!board.piece(position).isThereAnyPossibleMove()){
+			throw new ChessException("There is no possible moves for chosen piece");
+		}
 	}
 	
 	private Piece makeMove(Position source, Position target){
